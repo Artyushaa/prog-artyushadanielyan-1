17d24319f2d883187c03d2f3060c7c0f4fece5f9
Artyusha <artyushadanielyan@gmail.com>
2023-01-04 12:57:01 +0400

    rename shopingCartAPI

--- day-114/products/features/shoppingCart/shoppingCartAPI.js
+++ day-114/products/features/shoppingCart/shoppingCartAPI.js
@@ -1,17 +1,13 @@
 import axios from 'axios'
 
-async function asyncRequestDeleteCart(url, cartId) {
+export async function asyncRequestDeleteCart(url, cartId) {
     let token = localStorage.getItem('token')
-    var myHeaders = new Headers();
-    myHeaders.append("Cookie", "session=s%3A2Uga7S3EoPMYFYSp28DXkkA8S8uw8wBy.9Qc2KwSU7gSmH49cu2juNVNRZqvxgD4fqosITwnAgqQ");
-    myHeaders.append("Authorization", `Bearer ${token}`);
     let res = await axios.delete(`${url}/${cartId}`, {
-        headers: myHeaders,
-        body: JSON.stringify(cartId),
-        redirect: 'follow'
+        headers: { Authorization: `Bearer ${token}` },
+        data: {
+            cartId
+        },
     })
     let result = await res.json()
     return result
 } 
-
-export default asyncRequestDeleteCart
